7a8c6818b0cad9c5965b724d39d9da1fae54ba9c
iswifters <zeeondra@gmail.com>
2024-04-01 13:13:32 +0100

    Initial Commit

---
+++ Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by MAC on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
